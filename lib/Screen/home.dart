f50674df7410858a73a6e38feb0f9d5ce93e5e50
Subramanyambandi <subramanyambandi8@gmail.com>
2023-12-06 13:58:15 +0530

    master

---
+++ lib/Screen/home.dart
@@ -0,0 +1,102 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<dynamic> data = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    final response = await http
+        .get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        data = json.decode(response.body);
+      });
+    } else {
+      print('Failed to load data: ${response.statusCode}');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Simple API '),
+      ),
+      body: Center(
+        child: data.isEmpty
+            ? const CircularProgressIndicator() // Display a loading indicator while fetching data
+            : ListView.builder(
+                itemCount: data.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 150,
+                          width: 300,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: NetworkImage(data[index]['url'])),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              height: 40,
+                              width: 40,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  image: DecorationImage(
+                                      image: NetworkImage(
+                                          data[index]['thumbnailUrl']))),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  width: 400,
+                                  padding: const EdgeInsets.all(10),
+                                  child: Text(
+                                    data[index]['title'],
+                                    // overflow: TextOverflow.ellipsis,
+                                    // maxLines: 1,
+                                    softWrap: true,
+                                    style: const TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Text(data[index]['albumId'].toString()),
+                                Text(data[index]['id'].toString()),
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+      ),
+    );
+  }
+}
